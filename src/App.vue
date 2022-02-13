2e2c515c99f0e1b8f1f2a6ab2388b8a356758e5a
Leooeloel <1826234293@qq.com>
2022-02-13 21:46:25 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="width: 100vw; height: 100vh" ref="whiteboard"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import { createFastboard, mount } from "@netless/fastboard";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      uuid: "",
+      appId: "i_dKMIzIEeyy2vEM-8rsrA/oBccCLz1aNHryw",
+      roomToken: "",
+      app: "",
+      url: "https://api.netless.link/v5/rooms",
+      tokenUrl: "https://api.netless.link/v5/tokens/rooms/",
+      sdkToken: "NETLESSSDK_YWs9cXVnUmllSWdWVW5zT1FOWCZub25jZT0yOWY4MGQwMC04Y2M5LTExZWMtYjJkYS1mMTBjZmJjYWVjYWMmcm9sZT0wJnNpZz01MDZjMjhiNDliNmU4YTEzMzg2YTU0MjhmMDRiYjdlMzkzYjQ5Mzg5YjRlMGI1NTU5MzljNDZhMjEyMmExZjQz"
+    }
+  },
+  async mounted() {
+    console.log(this.$refs.whiteboard);
+    const uuidRes = await this.getUUid();
+    this.uuid = uuidRes.data.uuid;
+
+    const tokenRes = await this.getToken();
+    this.roomToken = tokenRes.data
+
+    console.log(this.uuid, this.roomToken);
+
+    await this.mountFastboard(this.$refs.whiteboard);
+  },
+  methods: {
+    async getUUid() {
+      return axios.post(this.url, {}, {
+        headers: {
+          "content-type": "application/json",
+          "region": "cn-hz",
+          "token": this.sdkToken,
+        }
+      })
+    },
+    getToken() {
+      return axios.post(this.tokenUrl + this.uuid, {
+          lifespan: 0,
+          role: "admin"
+        }, {
+        headers: {
+          "content-type": "application/json",
+          "region": "cn-hz",
+          "token": this.sdkToken,
+        }
+      });
+    },
+  async mountFastboard(div) {
+    this.app = await createFastboard({
+      // [1]
+      sdkConfig: {
+        appIdentifier: this.appId 
+      },
+      // [2]
+      joinRoom: {
+        uid: "",
+        uuid: this.uuid,
+        roomToken: this.roomToken,
+      },
+      // [3]
+      managerConfig: {
+        cursor: true,
+      },
+    });
+    return mount(this.app, div);
+  },
   }
 }
+
 </script>
 
 <style>
